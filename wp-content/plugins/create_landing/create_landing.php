dff70ff4f800e08017d1596559def80d79ab225b
Nick Taporuk <nictaporuk@yandex.ru>
2015-01-05 20:17:36 +0200

    init landing

--- wp-content/plugins/create_landing/create_landing.php
+++ wp-content/plugins/create_landing/create_landing.php
@@ -1,42 +1,18 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: nicktaporuk
- * Date: 04.01.15
- * Time: 1:19
- */
-<?php
-/**
  * @package Akismet
  */
 /*
-Plugin Name: Akismet
-Plugin URI: http://akismet.com/
-Description: Used by millions, Akismet is quite possibly the best way in the world to <strong>protect your blog from comment and trackback spam</strong>. It keeps your site protected from spam even while you sleep. To get started: 1) Click the "Activate" link to the left of this description, 2) <a href="http://akismet.com/get/">Sign up for an Akismet API key</a>, and 3) Go to your Akismet configuration page, and save your API key.
-Version: 3.0.4
-Author: Automattic
-Author URI: http://automattic.com/wordpress-plugins/
-License: GPLv2 or later
-Text Domain: akismet
+Plugin Name: Landing Creator
+Plugin URI: nictaporuk@yandex.ru
+Description: Landing Creator
+Version: 0.0.1
+Author: Nick Taporuk
+Author URI: nictaporuk@yandex.ru
+License: MIT
+Text Domain: landing_create
 */
 
-/*
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
-
-// Make sure we don't expose any info if called directly
 if ( !function_exists( 'add_action' ) ) {
     echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
     exit;
---
+++ wp-content/themes/landings/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nicktaporuk
+ * Date: 05.01.15
+ * Time: 20:05
+ */ 
